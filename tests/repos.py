d07558144e6d02674db0532d9533f806b715abd5
Alex Mueller <amueller474@gmail.com>
2025-09-15 23:36:28 -0400

    Adds sample repo list

---
+++ tests/repos.py
@@ -0,0 +1,7 @@
+repos = {
+    "asottile/astpretty": ("5b68c7e", "5a8296f"),
+    "numpy/numpy": ("dca33b3", "5f82966"),
+    "pallets/click": ("93c6966", "e11a1ef"),
+    "scipy/scipy": ("c2220c0", "4ca6dd9"),
+    "yaml/pyyaml": ("48838a3", "a2d19c0"),
+}
